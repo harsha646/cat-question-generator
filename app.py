9d3aa776269512f81c23f7e9c5722df1e03e7781
harsha646 <harshasplunk2@gmail.com>
2025-03-30 05:01:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def home():
     return "CAT Question Generator API is running!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
